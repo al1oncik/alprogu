e0e6edd97327fe651dd3b9513dac5430881fbaf9
al1oncik <alenurliuk@gmail.com>
2022-12-28 11:54:12 +0200

    changes

---
+++ main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'main/base.html')
+
+
+def registration(request):
+    return HttpResponse("registration page")
