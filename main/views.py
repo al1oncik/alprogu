50903c97f2adfb9facc1c58d76d1a96ce6e2e7e6
al1oncik <alenurliuk@gmail.com>
2022-12-29 14:06:43 +0200

    some changes

--- main/views.py
+++ main/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
@@ -7,4 +6,4 @@ def index(request):
 
 
 def registration(request):
-    return HttpResponse("registration page")
+    return render(request, 'main/registration.html')
