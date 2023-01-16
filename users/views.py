ade304d7b1198a803624e29e52632c1cf9855363
al1oncik <alenurliuk@gmail.com>
2023-01-16 22:13:07 +0200

    created basic registration page without design, but it is not working as well

--- users/views.py
+++ users/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from users.forms import UserRegistrationForm
 
 
 def login(request):
@@ -10,4 +13,13 @@ def logout(request):
 
 
 def register(request):
-    ...
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserRegistrationForm()
+    context = {'form': form}
+
+    return render(request, 'users/register.html', context)
