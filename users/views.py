3840b995911a1fab770d9eabf4453a6e6a3c0578
al1oncik <alenurliuk@gmail.com>
2023-01-17 21:34:25 +0200

    some changes

--- users/views.py
+++ users/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
 from django.urls import reverse
+from django.contrib import auth
 from django.http import HttpResponseRedirect
-from users.forms import UserRegistrationForm
+from users.forms import UserRegistrationForm, UserLoginForm
 
 
 def login(request):
-    ...
+    if request.method == "POST":
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
 
 
 def logout(request):
-    ...
+    auth.logout(request.user)
+    return HttpResponseRedirect(reverse('index'))
 
 
 def register(request):
