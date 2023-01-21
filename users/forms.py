b8d6b34d2c0a4465b79d940f39b435a7ff972d06
al1oncik <alenurliuk@gmail.com>
2023-01-21 15:47:02 +0200

    Bootstrap included

--- users/forms.py
+++ users/forms.py
@@ -5,11 +5,11 @@ from django import forms
 
 class UserLoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={
-        'class': 'username_input',
+        'class': 'username_input form-control',
         'placeholder': 'username',
     }))
     password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'class': 'password_input',
+        'class': 'password_input form-control',
         'placeholder': 'password',
     }))
 
@@ -20,27 +20,27 @@ class UserLoginForm(AuthenticationForm):
 
 class UserRegistrationForm(UserCreationForm):
     first_name = forms.CharField(widget=forms.TextInput(attrs={
-        'class': 'user_input',
+        'class': 'user_input form-control',
         'placeholder': 'First name',
     }))
     last_name = forms.CharField(widget=forms.TextInput(attrs={
-        'class': 'user_input',
+        'class': 'user_input form-control',
         'placeholder': 'Last name',
     }))
     username = forms.CharField(widget=forms.TextInput(attrs={
-        'class': 'user_input',
+        'class': 'user_input form-control',
         'placeholder': 'Username',
     }))
     email = forms.EmailField(widget=forms.EmailInput(attrs={
-        'class': 'user_input',
+        'class': 'user_input form-control',
         'placeholder': 'email',
     }))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={
-        'class': 'user_input',
+        'class': 'user_input form-control',
         'placeholder': 'create password',
     }))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={
-        'class': 'user_input',
+        'class': 'user_input form-control',
         'placeholder': 'verify password',
     }))
 
